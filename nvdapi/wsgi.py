99fb82e03e28db63e1472fa62b4c4b311add2d84
Daniel Vaca <dvaca@hispasec.com>
2015-12-22 13:09:04 +0100

    Name fix on wsgi file

--- nvdapi/wsgi.py
+++ nvdapi/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cps.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nvdapi.settings")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
