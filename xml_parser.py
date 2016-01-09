32bfa4af26ee019e8ff6257b324f4cebcccc4d25
Daniel Vaca <dvaca@hispasec.com>
2016-01-09 11:50:23 +0100

    Naive dates added to parser

--- xml_parser.py
+++ xml_parser.py
@@ -17,135 +17,146 @@ from django.conf import settings
 api = twitter.Api(consumer_key = settings.TWITTER_CONSUMER_KEY,
                     consumer_secret = settings.TWITTER_CONSUMER_SECRET,
                     access_token_key = settings.TWITTER_ACCESS_TOKEN,
-					access_token_secret = settings.TWITTER_ACCESS_TOKEN_SECRET)
+                    access_token_secret = settings.TWITTER_ACCESS_TOKEN_SECRET)
 
 def get_cve(entry):
-	return entry.attrib.get("id")
+    return entry.attrib.get("id")
 def get_published_datetime(entry):
-	for childs in entry:
-		if "published-datetime" in childs.tag:
-			return datetime.datetime.strptime(childs.text[:-10], "%Y-%m-%dT%H:%M:%S")
+    for childs in entry:
+        if "published-datetime" in childs.tag:
+            return datetime.datetime.strptime(childs.text[:-10], "%Y-%m-%dT%H:%M:%S")
 def get_last_modified_datetime(entry):
-	for childs in entry:
-		if "last-modified-datetime" in childs.tag:
-			return datetime.datetime.strptime(childs.text[:-10], "%Y-%m-%dT%H:%M:%S")
+    for childs in entry:
+        if "last-modified-datetime" in childs.tag:
+            return datetime.datetime.strptime(childs.text[:-10], "%Y-%m-%dT%H:%M:%S")
 def get_summary(entry):
-	for childs in entry:
-		if "summary" in childs.tag:
-			return childs.text
+    for childs in entry:
+        if "summary" in childs.tag:
+            return childs.text
 def get_score(entry):
-	for child in entry:
-		if "cvss" in child.tag:
-			base_metrics = child[0] #base_metrics
-			for aux in base_metrics:
-				if "score" in aux.tag:
-					return float(aux.text)
-	return float(0)
+    for child in entry:
+        if "cvss" in child.tag:
+            base_metrics = child[0] #base_metrics
+            for aux in base_metrics:
+                if "score" in aux.tag:
+                    return float(aux.text)
+    return float(0)
 def get_cvss_value(entry, value):
-	for child in entry:
-		if "cvss" in child.tag:
-			base_metrics = child[0] #base_metrics
-			for aux in base_metrics:
-				if value in aux.tag:
-					return aux.text
+    for child in entry:
+        if "cvss" in child.tag:
+            base_metrics = child[0] #base_metrics
+            for aux in base_metrics:
+                if value in aux.tag:
+                    return aux.text
 def get_cpes_affected(entry):
-	cpes = []
-	for child in entry:
-		if "vulnerable-software-list" in child.tag:
-			vulnerable_software = child
-			for cpe in vulnerable_software:
-				cpes.append(cpe.text)
-	return cpes
+    cpes = []
+    for child in entry:
+        if "vulnerable-software-list" in child.tag:
+            vulnerable_software = child
+            for cpe in vulnerable_software:
+                cpes.append(cpe.text)
+    return cpes
 def get_references(entry):
-	references = []
-	for child in entry:
-		if "references" in child.tag:
-			for aux in child:
-				if "reference" in aux.tag:
-					references.append(aux.attrib.get("href"))
-	return references
+    references = []
+    for child in entry:
+        if "references" in child.tag:
+            for aux in child:
+                if "reference" in aux.tag:
+                    references.append(aux.attrib.get("href"))
+    return references
 def process_entry(entry):
-	cve = get_cve(entry)
-	published_on = get_published_datetime(entry)
-	modified_on = get_last_modified_datetime(entry)
-	summary = get_summary(entry)
-	score = get_score(entry)
-	access_vector = get_cvss_value(entry, 'access-vector')
-	access_complexity = get_cvss_value(entry, 'access-complexity')
-	authentication = get_cvss_value(entry, 'authentication')
-	confidentiality_impact = get_cvss_value(entry, 'confidentiality-impact')
-	integrity_impact = get_cvss_value(entry, 'integrity-impact')
-	availability_impact = get_cvss_value(entry, 'availability-impact')
-	cpes = get_cpes_affected(entry)
-	references = get_references(entry)
+    cve = get_cve(entry)
+    published_on = timezone.make_aware(get_published_datetime(entry), timezone.get_default_timezone())
+    modified_on = timezone.make_aware(get_last_modified_datetime(entry), timezone.get_default_timezone())
+    summary = get_summary(entry)
+    score = get_score(entry)
+    access_vector = get_cvss_value(entry, 'access-vector')
+    access_complexity = get_cvss_value(entry, 'access-complexity')
+    authentication = get_cvss_value(entry, 'authentication')
+    confidentiality_impact = get_cvss_value(entry, 'confidentiality-impact')
+    integrity_impact = get_cvss_value(entry, 'integrity-impact')
+    availability_impact = get_cvss_value(entry, 'availability-impact')
+    cpes = get_cpes_affected(entry)
+    references = get_references(entry)
 
-	vuln, created = Vulnerability.objects.get_or_create(cve=cve, defaults={
-							"released_on":published_on, 
-							"modified_on":modified_on, 
-							"description":summary,
-							"score":score,
-							"access_vector":access_vector,
-							"access_complexity":access_complexity,
-							"authentication":authentication,
-							"confidentiality_impact":confidentiality_impact,
-							"integrity_impact":integrity_impact,
-							"availability_impact":availability_impact})
+    vuln, created = Vulnerability.objects.get_or_create(cve=cve, defaults={
+                            "released_on":published_on, 
+                            "modified_on":modified_on, 
+                            "description":summary,
+                            "score":score,
+                            "access_vector":access_vector,
+                            "access_complexity":access_complexity,
+                            "authentication":authentication,
+                            "confidentiality_impact":confidentiality_impact,
+                            "integrity_impact":integrity_impact,
+                            "availability_impact":availability_impact})
 
+    if created:
+        print "CREATED %s" % vuln.cve
 
-	if not created:
-		vuln.modified_on = modified_on
-		vuln.description = summary
-		vuln.score = score
-		vuln.access_vector = access_vector
-		vuln.access_complexity = access_complexity
-		vuln.authentication = authentication
-		vuln.confidentiality_impact = confidentiality_impact
-		vuln.integrity_impact = integrity_impact
-		vuln.availability_impact = availability_impact
-		vuln.save()
+    if not created and vuln.modified_on != modified_on:
+        vuln.modified_on = modified_on
+        vuln.description = summary
+        vuln.score = score
+        vuln.access_vector = access_vector
+        vuln.access_complexity = access_complexity
+        vuln.authentication = authentication
+        vuln.confidentiality_impact = confidentiality_impact
+        vuln.integrity_impact = integrity_impact
+        vuln.availability_impact = availability_impact
+        vuln.save()
+        print "UPDATED %s" % vuln.cve
 
-	count_before = vuln.product_version.count()
+    product_version_added = 0
 
-	#Process CPEs
-	for cpe in cpes:
-		aux = cpe.replace("cpe:/","").split(":")
-		
-		try:
-			product, created = Product.objects.get_or_create(vendor=aux[1].replace("_"," "), 
-							name=aux[2].replace("_"," "), 
-							defaults={"type":aux[0], })
-		except Exception, e:
-			continue
+    #Process CPEs
+    for cpe in cpes:
+        aux = cpe.replace("cpe:/","").split(":")
+        
+        try:
+            product, created = Product.objects.get_or_create(vendor=aux[1].replace("_"," "), 
+                            name=aux[2].replace("_"," "), 
+                            defaults={"type":aux[0], })
+        except Exception, e:
+            continue
 
-		try:
-			version = aux[3]
-		except Exception, e:
-			version = ""
-		try:
-			product_version, created = ProductVersion.objects.get_or_create(product=product, version=version)
-		except Exception, e:
-			product_version = ProductVersion.objects.get(product=product, version=version)
-		vuln.product_version.add(product_version)
+        try:
+            version = aux[3]
+        except Exception, e:
+            version = ""
+        try:
+            product_version, created = ProductVersion.objects.get_or_create(product=product, version=version)
+            if created:
+                product_version_added = product_version_added + 1
+        except Exception, e:
+            print e
+            product_version = ProductVersion.objects.get(product=product, version=version)
+        vuln.product_version.add(product_version)
 
-	count_after = vuln.product_version.count()
-	if count_after > count_before:
-		products = [x.product.name for x in vuln.product_version.all()]
-		products = list(set(products))
-		link = "https://web.nvd.nist.gov/view/vuln/detail?vulnId=%s" % vuln.cve
+    if product_version_added > 0:
+        products = [x.product.name for x in vuln.product_version.all()]
+        products = list(set(products))
+        link = "https://web.nvd.nist.gov/view/vuln/detail?vulnId=%s" % vuln.cve
 
-		tweet = []
-		tweet.append(vuln.cve)
-		tweet.append("(%s)" % ', '.join(products))
-		tweet.append(link)
-		tweet = " ".join(tweet)
-		status = api.PostUpdate(tweet)
+        tweet = []
+        tweet.append(vuln.cve)
+        tweet.append("(%s)" % ', '.join(products))
+        tweet.append(link)
+        tweet = " ".join(tweet)
+        try:
+            status = api.PostUpdate(tweet)
+        except:
+            products = [x.product.name for x in vuln.product_version.all()]
+            products = products[0]
+            tweet = []
+            tweet.append(vuln.cve)
+            tweet.append("(%s)" % ', '.join(products))
+            tweet.append(link)
+            tweet = " ".join(tweet)
+            status = api.PostUpdate(tweet)
 
-	#Process references
-	for url in references:
-		VulnerabilitySource.objects.get_or_create(vulnerability=vuln, url=url)
+    #Process references
+    for url in references:
+        VulnerabilitySource.objects.get_or_create(vulnerability=vuln, url=url)
 
-	print vuln
-# for i in range(14,16):
-# 	e = xml.etree.ElementTree.parse('nvdcve-2.0-20%s.xml' % format(i,'02')).getroot()
-# 	for entry in e:
-# 		process_entry(entry)
+    print vuln
