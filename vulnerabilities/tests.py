7c6bc7292c1d21293164c3e08708e170ac8e8121
Daniel Vaca <dvaca@hispasec.com>
2016-04-19 20:49:48 +0200

    commands unittests

--- vulnerabilities/tests.py
+++ vulnerabilities/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from vulnerabilities.models import *
+from django.core.management import call_command
 
 # Create your tests here.
+class CommandsTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_recent_nvd(self):
+        call_command('nvdrecent')
+
+    def test_modified_nvd(self):
+        call_command('nvdmodified')
