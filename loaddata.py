802885562b45a1c9d9b7ac2166df61f3f0f5350c
Adi Catana <adrian.catana15@imperial.ac.uk>
2017-10-18 17:12:33 +0100

    Updated nvd links, requirements for the env, deprecated QUERY_PARAMS to query_params and removed twitter from xml_parser

--- loaddata.py
+++ loaddata.py
@@ -12,7 +12,7 @@ range_from = 2002
 range_to = 2016
 
 for i in range(range_from, range_to+1):
-	os.system("wget -O /tmp/aux.zip http://static.nvd.nist.gov/feeds/xml/cve/nvdcve-2.0-%i.xml.zip" % i)
+	os.system("wget -O /tmp/aux.zip https://static.nvd.nist.gov/feeds/xml/cve/2.0/nvdcve-2.0-%i.xml.zip" % i)
 	os.system("unzip -o /tmp/aux.zip -d /tmp/")
 	e = xml.etree.ElementTree.parse('/tmp/nvdcve-2.0-%i.xml' % i).getroot()
 	for entry in e:
